088ed4c824b2b868e870e30f324969a2d4d64ad1
dariki1 <james_atkins97@hotmail.com>
2020-01-10 13:33:49 +1100

    Button names are now based on definition file
    Buttons now group according to the displayName property of the files.json the file is in
    Name fallbacks to the files name if there is no definition for the file

--- public/script.ts
+++ public/script.ts
@@ -8,7 +8,7 @@ async function loadAudioPath(path: string): Promise<string[]> {
 			let ret: string[] = [];
 			let elements = (<Document>(value.responseXML)).children[0].children[1].children[1].children[1].children;
 			for (let x = 1; x < elements.length; x++) {
-				if (elements[x].children[0].children[0].innerHTML.match(/\.(mp3)$/)) {
+				if (elements[x].children[0].children[0].innerHTML.match(/\.(mp3|json)$/)) {
 					ret.push(path+"/"+elements[x].children[0].children[0].innerHTML);
 				}
 			}
@@ -17,12 +17,16 @@ async function loadAudioPath(path: string): Promise<string[]> {
 	});
 }
 
-async function loadAudio(sounds: {ambience: string[], soundEffects: string[]}) {
+async function loadAudio(sounds: {ambience: {sounds: string[], definitions: string}, soundEffects: {sounds: string[], definitions: string}}) {
 	let promises = <Promise<string[] | null | void>[]>[];
 	promises.push(loadAudioPath("/resources/sounds/ambience").then((value: string[]) => {
 		return new Promise((res) => {
 			for (let e of value) {
-				sounds.ambience.push(e);
+				if (e.match(/\.(json)$/)) {
+					sounds.ambience.definitions = e;
+					continue;
+				}
+				sounds.ambience.sounds.push(e);
 			}
 			res();
 		});		 
@@ -30,7 +34,11 @@ async function loadAudio(sounds: {ambience: string[], soundEffects: string[]}) {
 	promises.push(loadAudioPath("/resources/sounds/soundEffects").then((value: string[]) => {
 		return new Promise((res) => {
 			for (let e of value) {
-				sounds.soundEffects.push(e);
+				if (e.match(/\.(json)$/)) {
+					sounds.soundEffects.definitions = e;
+					continue;
+				}
+				sounds.soundEffects.sounds.push(e);
 			}
 			res();
 		});
@@ -63,9 +71,29 @@ function makeRequest(method: string, url: string, responseType: XMLHttpRequestRe
 	});
 }
 
-function addButtons(buttonParent: HTMLElement, sources: string[], audioParent: HTMLAudioElement) {	
+async function addButtons(buttonParent: HTMLElement, sources: string[], audioParent: HTMLAudioElement, definitionsURL: string) {
+	let definitions: {fileName:string, displayName:string}[] = [];
+
+	await makeRequest("GET", definitionsURL, "json").then((value: XMLHttpRequest) => {
+		definitions=value.response;
+	});
+
 	for (let source of sources) {
-		let name = ((source.match(/\/([a-z]| |-)+\d*\.mp3/i)||[])[0].match(/([a-z]| |-)+/i)||[])[0];
+		let name: string = "";		
+		let thisSound: string = (source.match(/\/([a-z]| |-)+\d*\.mp3/i)||[])[0].substr(1);
+
+		for (let definition of definitions) {
+			if (thisSound === definition.fileName) {
+				name = definition.displayName;
+				break;
+			}
+		}
+
+		if (name === "") {
+			console.warn("Loaded file with no definition; " + thisSound);
+			name = (thisSound.match(/([a-z]| |-)+/i)||[])[0];
+		}
+		
 		let b = buttons.filter((el) => el.button.innerHTML === name)[0];
 		if (b === undefined) {// If the sound has not been registered yet
 			let nBut = document.createElement("button");
@@ -81,7 +109,7 @@ function addButtons(buttonParent: HTMLElement, sources: string[], audioParent: H
 				(<HTMLSourceElement>audioParent.children[0]).src = b.sources[Math.floor(Math.random()*b.sources.length)];
 				audioParent.load();
 				audioParent.play();
-			}			
+			}
 			buttonParent.appendChild(b.button);
 		} else {
 			b.sources.push(source);
@@ -91,11 +119,17 @@ function addButtons(buttonParent: HTMLElement, sources: string[], audioParent: H
 
 (async function init() {
 	const sounds = {
-		ambience: <string[]>[],
-		soundEffects: <string[]>[]
+		ambience: {
+			sounds: <string[]>[],
+			definitions: <string>""
+		},
+		soundEffects: {
+			sounds: <string[]>[],
+			definitions: <string>""
+		}
 	};
 	await loadAudio(sounds);
 
-	addButtons(document.body, sounds.ambience, ambience);
-	addButtons(document.body, sounds.soundEffects, effect);
+	addButtons(document.body, sounds.ambience.sounds, ambience, sounds.ambience.definitions);
+	addButtons(document.body, sounds.soundEffects.sounds, effect, sounds.soundEffects.definitions);
 })();
